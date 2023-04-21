f64ab3f6979a9c0dad3aee485cd419fc7f3708bd
Alvaro Vazquez <alvarovazquez@MacBook-Air.local>
2023-04-21 01:46:22 -0600

    removed second event handler

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 var generateBtn = document.querySelector("#generate");
 
+
  let finalpassword = []
  let possibleCharacters=[]
+ // Possible character for the password
  let Specialchar = ['!','@','#','$','%','^','&','*']
  let numbers = [0,1,2,3,4,5,6,7,8,9]
  let upperCasechar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
  let lowerCasechar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+// function to randomize password array
 function random(arr) {
      var randomNumber = Math.floor(Math.random() * arr.length);
      return arr[randomNumber];
    }
 
  function generatePassword(){
-   
    var passwordLength = prompt('How long would you like your password?(8 to 128)')
 
    if (passwordLength < 8 || passwordLength > 128) {
@@ -30,7 +31,7 @@ function random(arr) {
 
    var includeNumbers = confirm('Do you want numbers?(yes/no)')
 
-
+// if conditions
   if(specialCharacters){
    possibleCharacters = possibleCharacters.concat(Specialchar)
   };
@@ -49,27 +50,36 @@ function random(arr) {
    let character = random(possibleCharacters);
    finalpassword.push(character);
  }
- generateBtn.addEventListener("click", function(){
-  document.querySelector(".card-body").placeholder="Your Secure Password"
-});
-  
- return finalpassword
+ let reset =function(){
+  let resetText = document.querySelector("#password");
+  resetText.value = ""
+  console.log('c;mon')
+  return resetText.value; 
+}
+
+return finalpassword 
 
  }
  
- generateBtn.addEventListener("click", function(){
-  document.querySelector(".card-body").placeholder="Your Secure Password"})
- // Get references to the #generate element
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
 
+ // Get references to the #generate element
+
+ // Write password to the #password input
+ 
+ function writePassword() {
+  let passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  
   passwordText.value = password.join('');
 
 }
+
 // Add event listener to generate button
 
-generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", function(){
-  document.querySelector(".card-body").innerHTML.placeholder="Your Secure Password"})
+generateBtn.addEventListener("click",writePassword)
+  
+  
+
+
+
+
